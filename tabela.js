a24d04267efb0513637ab65bd6d0e3314ce76bc8
JIsaquewillers <joaoisaquewillers@gmail.com>
2026-04-29 09:22:07 -0300

    .

--- tabela.js
+++ tabela.js
@@ -1,12 +1,34 @@
-for (let i = 0; i < colecaoElementos .length; i++) {
-    const tabela = document.querySelector('tabela')
-    tabela.innerHTML += `
-        <tr>
-            <td>${colecaoElementos[i][0]}</td>
-            <td>${colecaoElementos[i][1]}</td>
-            <td>${colecaoElementos[i][2]}</td>
-            <td>${colecaoElementos[i][3]}</td>
-            <td>${colecaoElementos[i][4]}</td>
-        </tr>
-    `;
+//cria uma variável para a tabela//
+const tabela = document.getElementById("tabela")
+
+//cria um laço de repetição para criar as 10 colunas da tabela//
+for(let i = 0; i < 10; i++){
+    const tr = document.createElement("tr")
+
+//cria um laço de repetição para criar as 18 linhas da tabela e colocar dentro das colunas//
+for(let i = 0; i < 18; i++){
+    const td = document.createElement("td")
+//coloca as linhas dentro das colunas//
+        tr.appendChild(td)
+    }
+//coloca as colunas dentro da tabela//
+    tabela.appendChild(tr)
 }
+
+//pega a colecaElementos e cria um laço de repetição com foreach.//
+//onde pega cada elemento(e)//
+colecaoElementos.forEach((e) => {
+//acha o elemento certo e suas linhas e colunas//
+// (-1) converte de base 1 para base 0 //
+    const td = tabela.rows[e.linha-1].cells[e.coluna-1]
+//coloca os itens h4,h4 e p dentro da linha com innerHTML//
+    td.innerHTML = `
+    <h4>${e.numeroAtomico}</h4>
+    <h3>${e.simbolo}</h3>   
+    <p>${e.nome}</p>
+    <p>${e.massaAtomica}</p>`
+    td.elemento = e
+})      
+
+        
+
